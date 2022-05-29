cce6346b9d45548c7a3d687092d937b2a2f05b81
mubaris-mansoor <mubaris.mansoor@science.christuniversity.in>
2022-05-29 23:24:02 +0530

    final

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("logo.png"));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,266 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.text.BreakIterator;
 
 public class HelloController {
     @FXML
+    public Label discountlabel;
+    public Label armyDiscountLabel;
+    public Label plusDiscountLabel;
+    public Label plusTotalLabel;
+
+
+
+    @FXML
     private Label welcomeText;
 
+
+    @FXML
+    public Button closeButton;
+
+    @FXML
+    public static int basic = 1200;
+    public static int plus = 2500;
+    @FXML
+    public static int total;
+    @FXML
+    public static int discount;
+    public static int discount_plus;
+
+    public static String Total="";
+    public static String discountString="";
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void doctors() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Doctors");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void appointment() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("appointment.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Appointment");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void health() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("health.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Health packages");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void home() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("HOME");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void basichealth() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("basichealth.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Basic Health");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void closeButtonAction() throws IOException {
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+//
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+//        Stage nstage = new Stage();
+//        nstage.setTitle("HOME");
+//        nstage.setScene(scene);
+//        nstage.show();
+    }
+
+    @FXML
+    protected void rani() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("rani.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Rani");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void santosh() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("santosh.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Santosh");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void jayashree() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("jayashree.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Jayashree");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void sanu() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sanu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Sanu");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void charis() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Chandy.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Charis");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void henry() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("henry.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Dr.Henry");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void basicplus() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("basicplus.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Basic Plus Health Package");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void booking() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("booking.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Booking");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void army_calc() {
+        Total = "";
+        discountlabel.setText(Total);
+        discount = basic*10/100;
+        total = basic-discount;
+        Total = new String (String.valueOf(total));
+        discountlabel.setText("₹"+Total);
+        discountString = new String("10%");
+        armyDiscountLabel.setText(discountString);
+    }
+
+    @FXML
+    protected void senior_calc() {
+        discount = basic*5/100;
+        total = basic-discount;
+        Total = new String (String.valueOf(total));
+        discountlabel.setText("₹"+Total);
+        discountString = new String("5%");
+        armyDiscountLabel.setText(discountString);
+    }
+
+    @FXML
+    protected void general_calc() {
+        discount = basic*2/100;
+        total = basic-discount;
+        Total = new String (String.valueOf(total));
+        discountlabel.setText("₹"+Total);
+        discountString = new String("2%");
+        armyDiscountLabel.setText(discountString);
+    }
+
+    @FXML
+    protected void senior_plus_calc() {
+        discount = plus*5/100;
+        total = plus-discount;
+        Total = new String (String.valueOf(total));
+        plusTotalLabel.setText("₹"+Total);
+        discountString = new String("5%");
+        plusDiscountLabel.setText(discountString);
+    }
+
+    @FXML
+    protected void army_plus_calc() {
+        discount = plus*15/100;
+        total = plus-discount;
+        Total = new String (String.valueOf(total));
+        plusTotalLabel.setText("₹"+Total);
+        discountString = new String("15%");
+        plusDiscountLabel.setText(discountString);
+    }
+
+    @FXML
+    protected void general_plus_calc() {
+        discount = plus*2/100;
+        total = plus-discount;
+        Total = new String (String.valueOf(total));
+        plusTotalLabel.setText("₹"+Total);
+        discountString = new String("2%");
+        plusDiscountLabel.setText(discountString);
+    }
+
 }
